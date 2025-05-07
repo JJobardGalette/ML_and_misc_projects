0e18fe0f3ec518d590b5c1567e9bf27a58d45ffc
Jojo <jjrjo@kth.se>
2025-05-07 22:03:42 +0200

    added kl ucb

--- Reinforcement_learning/bandits/KLUCBalgorithm.py
+++ Reinforcement_learning/bandits/KLUCBalgorithm.py
@@ -1,10 +1,31 @@
-"""UCB algorithm with functions to display what's happening cf. Auer et al. 2002"""
+"""KL-UCB algorithm with functions to display what's happening cf. Auer et al. 2002"""
 # %%
 from environment import Environment
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
-class UCB:
+def confidence_level(x):
+    if x == 1:
+        return 1e20
+    return np.log(x)+3*np.log(np.log(x))
+
+def kl_div(a, b):
+    return a*np.log(a/b) + (1-a)*np.log((1-a)/(1-b))
+
+def optimisation(n, t, theta, bounds=(1e-10, 1-1e-10)):
+    def objective(q):
+        if n*kl_div(theta, q) < confidence_level(t):
+            return -q
+        else:
+            return 1e6
+    optimisation_result = scipy.optimize.minimize_scalar(objective, bounds=bounds, method='bounded')
+    if not optimisation_result.success:
+        raise RuntimeError("optimisation failed!")
+    else:
+        return optimisation_result.x
+
+class KLUCB:
     def __init__(self, env:Environment):
         self.env = env
         self.empirical_means = np.ones(self.env.arms_number)*float('inf')
@@ -15,6 +36,7 @@ class UCB:
         self.empirical_means_history = []
         self.regret_history = [0]
         self.best_arm = np.argmax(env.means)
+        self.best_reward = max(self.env.means)
 
     def run(self, iterations):
         for i in range(iterations):
@@ -29,7 +51,8 @@ class UCB:
         for arm in range(self.env.arms_number):
             if self.rewards_list[arm]:
                 self.empirical_means[arm] = np.mean(self.rewards_list[arm])
-                self.b_a[arm] = self.empirical_means[arm] + np.sqrt(2*np.log(iteration+1)/len(self.rewards_list[arm]))
+                n_arm = len(self.rewards_list[arm])
+                self.b_a[arm] = optimisation(n_arm, self.env.time, self.empirical_means[arm])
         self.b_a_history.append(self.b_a.copy())
         self.empirical_means_history.append(self.empirical_means.copy())
 
@@ -39,6 +62,13 @@ class UCB:
         self.b_a = np.array([float('inf')]*self.env.arms_number)
         self.arm_history = []
         self.b_a_history = []
+    
+    def compute_regret(self, count):
+        arm_factor = 0
+        for arm in self.env.means:
+            if arm != self.best_reward:
+                arm_factor += (self.best_reward - arm)/kl_div(arm, self.best_reward)
+        return np.log(count[1:])*arm_factor
 
     def plot_convergence_result(self):
         plt.plot(self.empirical_means_history)
@@ -55,14 +85,14 @@ class UCB:
     def plot_regret(self):
         count = np.arange(0, len(self.arm_history))
         plt.plot(count, self.regret_history[1:])
-        theoretical_regret = 9 * np.sqrt(self.env.arms_number * count[1:] * np.log(count[1:])) + 8*self.env.arms_number/3
+        theoretical_regret = self.compute_regret(count)
         plt.plot(count[1:], theoretical_regret, linestyle='--')
-        plt.text(0, 1, "as one can see, this bound is not the best")
+        # plt.text(0, 1, "as one can see, this bound is not the best")
         plt.show()
 
 if __name__ == '__main__':
-    env = Environment([0.6, 0.5, 0.8, 0.9, 0.3, 0.31])
-    ucb = UCB(env)
-    ucb.run(10000)
-    ucb.plot_convergence_result()
-    ucb.plot_regret()
+    env = Environment([0.6, 0.4])
+    klucb = KLUCB(env)
+    klucb.run(10000)
+    klucb.plot_convergence_result()
+    klucb.plot_regret()
--- Reinforcement_learning/bandits/environment.py
+++ Reinforcement_learning/bandits/environment.py
@@ -7,6 +7,8 @@ class Environment:
     def __init__(self, means):
         self.means = means
         self.arms_number = len(self.means)
+        self.time = 0
 
     def play_arm(self, i):
+        self.time += 1
         return np.random.random() < self.means[i]
